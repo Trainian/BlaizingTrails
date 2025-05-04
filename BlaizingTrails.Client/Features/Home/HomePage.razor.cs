01fa901539ee01acb000c8b7127cb3070fb554bf
trainian <dimon007@inbox.ru>
2025-05-04 21:56:52 +0300

    Добавьте файлы проекта.

---
+++ BlaizingTrails.Client/Features/Home/HomePage.razor.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Http.Json;
+
+namespace BlaizingTrails.Client.Features.Home
+{
+    public partial class HomePage
+    {
+        private IEnumerable<Trail>? _trails;
+        private Trail? _selectedTrail;
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                _trails = await Http.GetFromJsonAsync<IEnumerable<Trail>>("trails/trail-data.json");
+            }
+            catch (HttpRequestException ex)
+            {
+                Console.WriteLine($"There was a problem loading trail data: {ex.Message}");
+            }
+        }
+
+        private void HandleTrailSelected(Trail trail) => _selectedTrail = trail;
+    }
+}
